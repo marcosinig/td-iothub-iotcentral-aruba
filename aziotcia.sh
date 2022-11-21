567a05f55bbbc5f11e126bca1a2d9dfd3add048a
Marco Sinigaglia <marco.sinigaglia@techdata.com>
2022-11-21 11:23:46 +0000

    fix enrollment

--- aziotcia.sh
+++ aziotcia.sh
@@ -67,13 +67,13 @@ if [ $IS_IOTHUB_DEPLOY_STR == "true" ];then
   DPS_RAND_SUFFIX='' #an contain only alphanumeric
   DPS_NAME=$(printf "DPS%s%s" "$IOT_HUB_HOST_NAME" "$DPS_RAND_SUFFIX")
   DPS_CREATE_ANS=$(az iot dps create --name $DPS_NAME --resource-group $RESOURCE_GROUP_NAME)
-  DPS_IDSCOPE=$(echo $DPS_CREATE_ANS | $jq '.properties.idScope' |  sed 's/^"\(.*\)".*/\1/')
-  DPS_GLOBAL_ENDPOINT=$(echo $DPS_CREATE_ANS | $jq '.properties.deviceProvisioningHostName' |  sed 's/^"\(.*\)".*/\1/')
+  DPS_IDSCOPE=$(echo $DPS_CREATE_ANS | jq '.properties.idScope' |  sed 's/^"\(.*\)".*/\1/')
+  DPS_GLOBAL_ENDPOINT=$(echo $DPS_CREATE_ANS | jq '.properties.deviceProvisioningHostName' |  sed 's/^"\(.*\)".*/\1/')
   
   az iot dps linked-hub create --dps-name $DPS_NAME --resource-group $RESOURCE_GROUP_NAME --connection-string $IOT_HUB_CONNECTION_STRING
-  DPS_ENROLMENT_PRIMARY_KEY=$(cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-88} | head -n 1)
-  DPS_ENROLMENT_SECONDARY_KEY=$(cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-88} | head -n 1)
-  az iot dps enrollment-group create -g $RESOURCE_GROUP_NAME --dps-name  $DPS_NAME --enrollment-id $DPS_ENROLMENTID --primary-key $DPS_ENROLMENT_PRIMARY_KEY  --secondary-key $DPS_ENROLMENT_SECONDARY_KEY
+  DPS_ENROLMENT_PRIMARY_KEY=$(dd if=/dev/urandom bs=56 count=1 status=none | base64)
+  DPS_ENROLMENT_SECONDARY_KEY=$(dd if=/dev/urandom bs=56 count=1 status=none | base64)
+  az iot dps enrollment-group create -g $RESOURCE_GROUP_NAME --dps-name  $DPS_NAME --enrollment-id 'arubaenrollmentid' --primary-key $DPS_ENROLMENT_PRIMARY_KEY  --secondary-key $DPS_ENROLMENT_SECONDARY_KEY
   echo "End iot Hub"
 else
   echo "Deploy iot Central"
